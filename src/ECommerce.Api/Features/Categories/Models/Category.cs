8192b955469560ac2b21f257fad42fb7fca1e8ea
Amir-Webdev <fullstack.dev.amir@gmail.com>
2026-04-28 12:38:40 +0330

    Updated Category Model to Implement BaseEntity

--- src/ECommerce.Api/Features/Categories/Models/Category.cs
+++ src/ECommerce.Api/Features/Categories/Models/Category.cs
@@ -1,14 +1,10 @@
+using ECommerce.Api.Common.Models;
+
 namespace ECommerce.Api.Features.Categories.Models;
 
-public class Category
+public class Category : BaseEntity
 {
-    public int Id { get; set; }
-
     public string Name { get; set; } = string.Empty;
 
     public string? Description { get; set; }
-
-    public bool IsActive { get; set; } = true;
-
-    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
 }
